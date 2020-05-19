5285915393c43a03bb9bd494d53951baea7a31e5
Sam Watts <samjwatts98@gmail.com>
2020-05-19 21:51:07 +0100

    Remove commented code.. whoops

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TileContainer, TimeOfDayTile, TileTheme } from './components/styles.css';
+import { TileContainer } from './components/styles.css';
 import SunriseTile from './components/SunriseTile';
 import Header from './components/Header';
 
@@ -12,12 +12,6 @@ function App() {
         <main>
           <TileContainer>
             <SunriseTile />
-            {/* <TimeOfDayTile theme={TileTheme.daytime} />
-            <TimeOfDayTile theme={TileTheme.sunset} />
-            <TimeOfDayTile theme={TileTheme.night} />
-            <TimeOfDayTile theme={TileTheme.daytime} />
-            <TimeOfDayTile theme={TileTheme.sunset} />
-            <TimeOfDayTile theme={TileTheme.night} /> */}
           </TileContainer>
         </main>
       </>
